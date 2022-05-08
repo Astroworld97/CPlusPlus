5631eb5753348f3783205c75835edf363e320152
iagonzal97 <iagonzal97@gmail.com>
2022-05-07 19:41:42 -0500

    restarted git practice

---
+++ C++Practice/C++PracticeNew/primer.cpp
@@ -0,0 +1,10 @@
+//Primer programa en C++
+//¡Si puedes imaginarlo, puedes programarlo!
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hola mundo :D" << endl; // OR: cout << "Hola mundo :D\n"
+    return 0;
+}
---
+++ C++Practice/C++PracticeNew/youtube.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //defines iostream
+//note: a namespace is like a library
+int main() //main function
+{
+    std::cout << "Hello World\n";
+    return 0; //just so the application works. This line is optional, it is called implicitly anyway. Instead of zero, some main functions might instead return EXIT_SUCCESS
+}
